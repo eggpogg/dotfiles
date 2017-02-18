e09c7f0556d7aaedee95d0e50e3dbaff58d152e9
kiharekato <hareki@Rigel.local>
2017-02-19 00:01:16 +0900

    first dotfile

---
+++ dotfilesInstaller.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+ln -snf ~/dotfiles/.gvimrc ~/.gvimrc
+ln -snf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -snf ~/dotfiles/.viminfo ~/.viminfo
+ln -snf ~/dotfiles/.vimrc ~/.vimrc
+ln -snf ~/dotfiles/.zshrc ~/.zshrc
